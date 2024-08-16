c61fefde182b40c1b2fbe1b27be2d8583140a83d
Jack Williams <jwilliams@MacBook-Pro.local>
2024-08-15 20:13:36 -0400

    Add missing app

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+//App.js
+
+import './App.css';
+import SignIn from './components/Signin.js';
+//import LogoutButton from './components/LogoutButton.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <>
+            <SignIn />
+        </>
+    );
+}
+
+export default App;
